68a0180f52e947d057c65231d6fcff7c4820e7ba
AlexRus01 <alex.rus11111@gmail.com>
2024-02-04 23:53:50 +0200

    Form proprietate

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -2,6 +2,11 @@ import {useState, useEffect, useRef} from 'react'
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
 import {useNavigate} from 'react-router-dom'
 import Spinner from '../components/Spinner'
+import {getStorage, ref, uploadBytesResumable, getDownloadURL,} from 'firebase/storage'
+import {db} from '../firebase.config'
+import {v4 as uuidv4} from 'uuid'
+import {toast} from 'react-toastify'
+import {addDoc, collection, serverTimestamp} from 'firebase/firestore'
 
 function CreateListing(){
     const [geolocationEnabled, setGeolocationEnabled] = useState(false)
@@ -47,15 +52,108 @@ function CreateListing(){
 
     }, [isMounted])
 
-    const onMutate = (e) => {}
 
-    const onSubmit = e => {
+
+    const onSubmit = async(e) => {
         e.preventDefault()
-    }
+        
+        console.log(formData)
+        console.log('alex')
+        let location = address
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage()
+                const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
 
+                const storageRef = ref(storage, 'images/' + fileName)
+
+                const uploadTask = uploadBytesResumable(storageRef, image)
+                
+                uploadTask.on('state_changed', 
+                        (snapshot) => {
+                            // Observe state change events such as progress, pause, and resume
+                            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                            console.log('Upload is ' + progress + '% done');
+                            switch (snapshot.state) {
+                            case 'paused':
+                                console.log('Upload is paused');
+                                break;
+                            case 'running':
+                                console.log('Upload is running');
+                                break;
+                            }
+                        }, 
+                        (error) => {
+                            reject(error)
+                        }, 
+                        () => {
+                            // Handle successful uploads on complete
+                            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                            resolve(downloadURL);
+                            });
+                        }
+                        );
+                        
+                        
+            })
+        }
+        
+        const imgUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+        ).catch(() => {
+            setLoading(false)
+            toast.error('Images not uploaded')
+            return
+        })
+        
+        
+        const formDataCopy = {
+            ...formData,
+            imgUrls,
+            timestamp: serverTimestamp()
+        }
+
+        delete formDataCopy.images
+        delete formDataCopy.address
+        location && (formDataCopy.location = location)
+        !formDataCopy.offer && delete formDataCopy.discountedPrice
+
+        const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
+        setLoading(false)
+        toast.success("Listing saved")
+        navigate(`/category/${formDataCopy.type}/${docRef.id}`)
+    }
+    const onMutate = (e) => {
+        let boolean = null
+
+        if(e.target.value === 'true'){
+            boolean = true
+        }
+        if(e.target.value === 'false'){
+            boolean = false
+        }
+
+        if(e.target.files){
+            setFormData((prevState) => ({
+                ...prevState,
+                images: e.target.files
+            }))
+        }
+        if(!e.target.files){
+            setFormData((prevState) => ({
+                ...prevState,
+                [e.target.id]: boolean ?? e.target.value
+            }))
+        }
+    }
     if(loading){
         return <Spinner />
     }
+
+
+
     return <div className='profile'>
         <header>
             <p className="pageHeader">Create a listing</p>
@@ -131,17 +229,17 @@ function CreateListing(){
         <label className='formLabel'>Regular price</label>
         <div className="formPricediv">
             <input className="formInputSmall" type="number" id="regularPrice" value={regularPrice}
-            onchange={onMutate} min='50' max='100000000' required />
+            onChange={onMutate} min='50' max='100000000' required />
             <p className="formPriceText">$</p>
         </div>
 
         <label className="formLabel">Images</label>
         <p className="imagesInfo">The first image will be the cover</p>
         <input className="formInputFile" type="file" id="images" onChange={onMutate} max='6' accept='.jpg,.png,.jpeg' multiple required />
-        
+        <button type="submit" className="primaryButton createListingButton">Create Listing</button>
         </form>
         
-        <button type="submit" className="primaryButton createListingButton">Create Listing</button>
+        
 
         </main>
     </div>
