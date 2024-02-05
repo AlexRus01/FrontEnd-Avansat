239c6dfe897ab08476367242e8219d3629aa075c
AlexRus01 <alex.rus11111@gmail.com>
2024-02-05 23:22:47 +0200

    Maiii multe updateuri

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -6,32 +6,26 @@ import bathIcon from '../assets/svg/bathtubIcon.svg'
 
 export default function ListingItem({listing, id}) {
   return (
-    <li className='categoryListing' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Link to={`/category/${listing.type}/${id}`} className='categoryListingLink'>
-       
-        <img
-          src={listing.imgUrls[0]}
-          alt={listing.name}
-          className='categoryListingImg'
-        />
-        
-        <div className="categoryListingDetails">
-            <p className="categoryListingLocation">
-                {listing.location}
-            </p>
-            <p className='categoryListingName'>{listing.name}</p>
-                
-            <p className="categoryListingPrice" style={{ textAlign: 'center' }}>
-            ${listing.offer ? listing.discountedPrice : listing.regularPrice}
-                {listing.type === 'rent' && ' / Month'}
-                
-            </p>
-        </div>
-        </Link>
-        
-        {
-          //onDelete && ( <DeleteIcon className='removeIcon' fill='rgb(231,76,60)' onClick={() => onDelete(listing.id, listing.name)} />)}
-        }
-    </li>
+<li className='categoryListing' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+  <Link to={`/category/${listing.type}/${id}`} className='categoryListingLink'>
+    <img
+      src={listing.imgUrls[0]}
+      alt={listing.name}
+      className='categoryListingImg'
+    />
+    <div className="categoryListingDetails" style={{ textAlign: 'center' }}>
+      <p className="categoryListingLocation">
+        {listing.location}
+      </p>
+      <p className='categoryListingName'>{listing.name}</p>
+      <p className="categoryListingPrice" style={{ textAlign: 'center', margin: '0 auto' }}>
+        ${listing.regularPrice}
+        {listing.type === 'rent' && ' / Month'}
+      </p>
+    </div>
+  </Link>
+  {/* onDelete && (<DeleteIcon className='removeIcon' fill='rgb(231,76,60)' onClick={() => onDelete(listing.id, listing.name)} />)} */}
+</li>
+
   )
 }
